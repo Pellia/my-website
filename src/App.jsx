94cf51a653d21e7a158b263396f9fe806c9e5699
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-06-18 15:55:34 +0200

    feat: added simple base page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
 const App = () => {
-    return <div className="text-4xl font-bold">Hello World!</div>;
+  return (
+    <main className="flex h-dvh flex-col items-center justify-center gap-4 bg-neutral-200 text-center text-neutral-700">
+      <h2 className="inline-block bg-gradient-to-r from-blue-400 via-indigo-400 to-violet-400 bg-clip-text text-4xl font-bold text-transparent">
+        Full Stack Developer
+      </h2>
+      <h1 className="text-7xl font-bold">Rupinder Singh</h1>
+      <ul className="mt-4 flex gap-6">
+        <li>
+          <a href="https://github.com/Pellia" target="_blank">
+            <img
+              src="/icons/github-dark.svg"
+              alt="github-icon"
+              className="h-auto w-8"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="www.linkedin.com/in/dev-rupinder-singh" target="_blank">
+            <img
+              src="/icons/linkedin.svg"
+              alt="linkedin-icon"
+              className="h-auto w-8"
+            />{" "}
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://bsky.app/profile/devpellia.bsky.social"
+            target="_blank"
+          >
+            <img
+              src="/icons/bluesky-dark.svg"
+              alt="bluesky-icon"
+              className="h-auto w-8"
+            />
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default App;
