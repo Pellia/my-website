9437be5c2982ffd98eb35b5e89ea422f2176face
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-09-03 02:04:41 +0200

    feat: added nav icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import Logo from "./Logo";
+import NavIcons from "./NavIcons";
 import Navigation from "./Navigation";
 
 const Header = () => {
     return (
-        <header className="bg-background/75 sticky top-0 h-16 backdrop-blur-md">
+        <header className="bg-background/75 border-border sticky top-0 right-0 left-0 z-50 h-16 border-b-1 backdrop-blur-md">
             <div className="container mx-auto flex h-full items-center justify-between gap-2 px-4">
                 <Logo />
                 <Navigation />
+                <NavIcons />
             </div>
         </header>
     );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <nav>
+        <nav className="flex justify-evenly">
             <ul className="flex gap-4">
                 <Link to="/">Home</Link>
-                <Link to="/articles">Articles</Link>
+                <Link to="/">Projects</Link>
+                <Link to="/">Resume</Link>
+                <Link to="/articles">Blog</Link>
                 <Link to="/resources">Resources</Link>
             </ul>
         </nav>
