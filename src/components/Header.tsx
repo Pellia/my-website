8bc8df4cd06b6407a422dbdc104087e366555ae5
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-09-04 13:42:30 +0200

    feat: added onscroll event to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
+import { useScroll, useMotionValueEvent } from "motion/react";
 import Logo from "./Logo";
 import NavIcons from "./NavIcons";
 import Navigation from "./Navigation";
 
 const Header = () => {
+    const { scrollY } = useScroll();
+
+    useMotionValueEvent(scrollY, "change", (current) => {
+        const header = document.getElementsByTagName("Header")[0];
+        if (current > 0) {
+            header.classList.add("border-b-1");
+        } else {
+            header.classList.remove("border-b-1");
+        }
+    });
+
     return (
-        <header className="bg-background/75 border-border sticky top-0 right-0 left-0 z-50 h-16 border-b-1 backdrop-blur-md">
+        <header className="bg-background/75 border-border sticky top-0 z-50 h-16 backdrop-blur-md">
             <div className="container mx-auto flex h-full items-center justify-between gap-2 px-4">
                 <Logo />
                 <Navigation />
