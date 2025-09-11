43d3a7d86605a7429945d2d216fbee1c1d3a3240
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-09-11 10:07:40 +0200

    ui: adjusted text size for reminder component

--- src/components/Reminder.tsx
+++ src/components/Reminder.tsx
@@ -1,10 +1,10 @@
-// import { IoIosInformationCircleOutline } from "react-icons/io";
-
 const Reminder = () => {
     return (
-        <div className="bg-background-secondary border-primary fixed right-0 bottom-0 m-4 flex w-1/2 items-center justify-center gap-4 rounded-lg border-1 p-3 lg:w-1/4">
-            {/* <IoIosInformationCircleOutline size={"5em"} className="w-[25%]" /> */}
-            <p>Site is currently active in Development. There will be new changes everyday!</p>
+        <div className="bg-background-secondary border-primary fixed right-0 bottom-0 m-4 items-center justify-center rounded-lg border-1 p-2 text-sm backdrop-blur-2xl">
+            <p>
+                Site is currently active in development. <br />
+                There will be new changes everyday!
+            </p>
         </div>
     );
 };
