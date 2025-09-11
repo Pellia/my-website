a9bec7121f2ea851c10e47b17270b69922d98542
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-09-11 13:55:40 +0200

    ui: fixed icon size in techstack component

--- src/data/techData.ts
+++ src/data/techData.ts
@@ -1,11 +1,11 @@
 // Main Tech items
 import type { IconType } from "react-icons";
-import { IoLogoReact, IoLogoJavascript, IoLogoCss3, IoLogoHtml5, IoLogoNodejs, IoLogoPython, IoLogoDocker, IoGitPullRequest, IoDocumentTextSharp } from "react-icons/io5";
+import { IoLogoReact, IoLogoJavascript, IoLogoCss3, IoLogoHtml5, IoLogoNodejs, IoLogoPython, IoLogoDocker, IoGitPullRequest, IoDocumentTextSharp, IoCodeSlashSharp } from "react-icons/io5";
 import { SiTypescript, SiTailwindcss, SiExpress, SiFastapi, SiServerless, SiPostgresql, SiOracle, SiClaris, SiMongodb } from "react-icons/si";
 import { FaDatabase, FaDiagramProject, FaLaptopCode, FaRegLightbulb, FaPersonRunning } from "react-icons/fa6";
 import { GrMysql } from "react-icons/gr";
 import { AiFillProduct, AiOutlineTeam } from "react-icons/ai";
-import { RiCodeAiFill, RiTestTubeFill } from "react-icons/ri";
+import { RiTestTubeFill } from "react-icons/ri";
 import { GrDocumentConfig } from "react-icons/gr";
 
 export interface TechDataItem {
@@ -13,7 +13,6 @@ export interface TechDataItem {
     name: string;
     icon: IconType;
     color?: string;
-    gradientColor?: [string, string];
 }
 
 export interface TechCategory {
@@ -41,7 +40,7 @@ export const techdata: TechCategory[] = [
         data: [
             { id: 0, name: "Node.js", icon: IoLogoNodejs, color: "#339933" },
             { id: 1, name: "Express.js", icon: SiExpress, color: "#000000" },
-            { id: 2, name: "Python", icon: IoLogoPython, color: "#3776AB", gradientColor: ["#3776AB", "#FFD43B"] },
+            { id: 2, name: "Python", icon: IoLogoPython, color: "#3776AB" },
             { id: 3, name: "FastAPI", icon: SiFastapi, color: "#009688" },
             { id: 4, name: "Docker", icon: IoLogoDocker, color: "#2496ED" },
             { id: 5, name: "Serverless", icon: SiServerless, color: "#FF434F" },
@@ -67,7 +66,7 @@ export const techdata: TechCategory[] = [
             { id: 1, name: "CI/CD", icon: SiTypescript, color: "#3178C6" },
             { id: 2, name: "Documentation", icon: IoDocumentTextSharp, color: "#F7DF1E" },
             { id: 3, name: "Git Flow", icon: IoGitPullRequest, color: "#F05032" },
-            { id: 4, name: "Clean Code", icon: RiCodeAiFill, color: "#3178C6" },
+            { id: 4, name: "Clean Code", icon: IoCodeSlashSharp, color: "#3178C6" },
             { id: 5, name: "Testing", icon: RiTestTubeFill, color: "#e5e5e5" },
         ],
     },
